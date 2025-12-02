028e09a8444e3761c9b99d9daa7676c1636aab84
BimBipUNu <trantai180506@gmail.com>
2025-12-02 21:09:02 +0700

    Fix database name config for production

--- src/config/config.js
+++ src/config/config.js
@@ -11,15 +11,22 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + '_test',
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql"
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + '_production',
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: false, // Tắt log SQL
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
