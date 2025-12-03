3c2585eda542bac834399ae947afb17d3698bd99
BimBipUNu <trantai180506@gmail.com>
2025-12-03 08:45:00 +0700

    fix jwt

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -4,6 +4,15 @@ require('dotenv').config();
 const verifyToken = (req, res, next) => {
   const token = req.headers['x-access-token'];
 
+  if (!token && req.headers.authorization) {
+    // Header Authorization thường có dạng: "Bearer eyJhbGci..."
+    // Chúng ta cần cắt bỏ chữ "Bearer " (7 ký tự đầu) để lấy token
+    const parts = req.headers.authorization.split(' ');
+    if (parts.length === 2 && parts[0] === 'Bearer') {
+      token = parts[1];
+    }
+  }
+
   if (!token) {
     return res.status(403).send({ message: 'No token provided!' });
   }
